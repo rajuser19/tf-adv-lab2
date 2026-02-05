b4e82e57691397559ae128371b99c5384b814722
raj <rnakkeeran04@gmail.com>
2026-02-05 21:15:02 +0000

    fmt

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-user5-feb26"
-    key            = "lab2/terraform.tfstate"
-    region         = "us-west-2"
-    use_lockfile   = true
+    bucket       = "tf-state-user5-feb26"
+    key          = "lab2/terraform.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
     #dynamodb_table = "terraform-locks"
   }
 }
